3b1207f919e9481056baec64cf0574fffcab9aa8
Prasanna Deshappriya <prasanna.14@cse.mrt.ac.lk>
2020-05-13 08:08:25 +0530

    minor changes

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', apiRouter);
 
 app.get('*',function (req, res) {
-  res.redirect('https://www.google.lk');
+  res.redirect('https://prasanna.alwaysdata.net');
 });
 
 // catch 404 and forward to error handler
